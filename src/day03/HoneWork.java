c7cf5ed772855b7681e03e259531b6bc5e759797
Rwenjie <1621317578@qq.com>
2020-06-17 10:51:37 +0800

    Rwenjie作业第一次项目结构上传

---
+++ src/day03/HoneWork.java
@@ -0,0 +1,14 @@
+package day03;
+
+/**
+ * @Author: Rwenjie
+ * @ProjectName: HomeWork
+ * @Description:
+ * @Date: 2020/6/17   10:47
+ **/
+public class HoneWork {
+
+    //打印99乘法表
+
+    //打印等腰三角形
+}
