adee216bb6db0c01ac36068a4c4a142b309339d5
Rwenjie <1621317578@qq.com>
2020-06-17 15:03:14 +0800

    2020/06/17实训第三天作业打印99乘法表和等腰三角形

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -1,21 +1,40 @@
 package day03;
 
+import java.util.Scanner;
+
 /**
  * @Author: Rwenjie
  * @ProjectName: HomeWork
  * @Description:
  * @Date: 2020/6/17   10:47
  **/
-public class HomeWork {
-
-    //打印99乘法表
-
-    //打印等腰三角形
+public  class HomeWork {
 
     public static void main(String[] args) {
-        int a, b;
-        System.out.println("测试pull");
-
+        //打印99乘法表
+        printf1();
+        //打印等腰三角形
+        printf2();
+    }
+    public static void printf1() {
+        for(int i=1; i<=9; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j+"*"+i+"="+(j*i)+"\t");
+            }
+            System.out.println();
+        }
     }
 
+    public static void printf2() {
+        int n = 10;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<=2*i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
