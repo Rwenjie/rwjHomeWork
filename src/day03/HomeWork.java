6423f301a29e8ec8cd6d6408c9b65a9362eba948
Rwenjie <1621317578@qq.com>
2020-06-17 19:19:19 +0800

    2020/06/17实训第三天作业

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -15,6 +15,8 @@ public  class HomeWork {
         printf1();
         //打印等腰三角形
         printf2();
+        //求100以内所有质数的和
+        primeSum();
     }
     private static void printf1() {
         for(int i=1; i<=9; i++){
@@ -36,5 +38,24 @@ public  class HomeWork {
             }
             System.out.println();
         }
+    }
+
+
+    private static boolean check(int n){
+        for(int i=2; i*i<=n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    private static void primeSum(){
+        int sum=0;
+        for(int i=2; i<=100; i++){
+            if(check(i)){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
     }
 }
